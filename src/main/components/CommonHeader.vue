ee5f09f5e8380ea31a500df8e17d36df81f2f578
Duye <superdoye0120@outlook.com>
2025-02-26 17:03:26 +0800

    feat: 场景切换现在支持本地存储了

--- src/main/components/CommonHeader.vue
+++ src/main/components/CommonHeader.vue
@@ -3,7 +3,11 @@
     <!-- 用户信息 -->
     <view class="user-info">
       <view class="avatar-wrapper">
-        <image class="avatar" src="/static/avatar.png" mode="aspectFill"></image>
+        <image
+          class="avatar"
+          src="/static/avatar.png"
+          mode="aspectFill"
+        ></image>
       </view>
       <view class="user-details">
         <view class="username">游客250221</view>
@@ -13,20 +17,76 @@
 
     <!-- Banner图像 -->
     <view class="banner">
-      <image class="banner-image" src="/static/banner.png" mode="widthFix"></image>
+      <image class="banner-image" :src="bannerSrc" mode="widthFix"></image>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 // 如果需要可以添加更多动态功能
+import { onLoad } from "@dcloudio/uni-app";
+import { SecneType } from "./type.d";
+
+const bannerSrc = ref<string>("");
+
+const getBannerSrcByKey = (key: SecneType): string => {
+  switch (key) {
+    case "hydrogenVehicle":
+      return "../static/changeScene/hydrogenVehicle.png";
+    case "gasElectricity":
+      return "../static/changeScene/gasElectricity.svg";
+    case "water":
+      return "../static/changeScene/water.svg";
+    case "bike":
+      return "../static/changeScene/bike.png";
+    case "zeroCarbonHouse":
+      return "../static/changeScene/zeroCarbonHouse.png";
+    default:
+      return "../static/changeScene/hydrogenVehicle.png";
+  }
+};
+
+// 场景变化处理函数
+const handleSceneChange = (sceneId: SecneType) => {
+  console.log("收到场景变化事件:", sceneId);
+  bannerSrc.value = getBannerSrcByKey(sceneId);
+};
+
+onMounted(() => {
+  // 在组件挂载时设置事件监听
+  uni.$on("sceneChange", handleSceneChange);
+});
+
+onUnmounted(() => {
+  // 在组件卸载时移除事件监听
+  uni.$off("sceneChange", handleSceneChange);
+});
+
+onLoad(() => {
+  console.log("CommonHeader 组件已加载");
+  // 初始化时从存储中获取场景ID
+  uni.getStorage({
+    key: "SceneContent",
+    success: function (res) {
+      if (res.data) {
+        console.log("从存储中获取到场景ID:", res.data);
+        bannerSrc.value = getBannerSrcByKey(res.data);
+      }
+    },
+    fail: function () {
+      console.log("未找到存储的场景ID，使用默认场景");
+      bannerSrc.value = getBannerSrcByKey("hydrogenVehicle");
+    },
+  });
+});
 </script>
 
 <style scoped>
 .common-header {
   width: 100%;
   position: relative;
-  background-color: #BBF3E6;
+  background-color: #bbf3e6;
 }
 
 .banner {
@@ -36,6 +96,8 @@
 .banner-image {
   width: 100%;
   display: block;
+  /* 等比例缩放 */
+  object-fit: cover;
 }
 
 .user-info {
@@ -75,6 +137,6 @@
 
 .points {
   font-size: 22rpx;
-  color: #4CAF50;
+  color: #4caf50;
 }
 </style>
---
+++ src/main/components/type.d.ts
@@ -0,0 +1,9 @@
+/**
+ * 场景类型
+ * hydrogenVehicle: 乘坐氢能交通工具
+ * gasElectricity: 走发电步道发电
+ * water: 喝海露纯净水
+ * bike: 体验零碳单车
+ * zeroCarbonHouse: 零碳小屋
+ */
+export type SecneType = 'hydrogenVehicle' | 'gasElectricity' | 'water' | 'bike' | 'zeroCarbonHouse'
